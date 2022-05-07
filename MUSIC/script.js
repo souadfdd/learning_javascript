c2e79374247d11175eeaf894f8851e7fbe91c079
Souad fdd <81853332+souadfdd@users.noreply.github.com>
2022-05-07 00:08:46 +0000

    Add files via upload

---
+++ MUSIC/script.js
@@ -0,0 +1,76 @@
+const musicContainer=document.querySelector('.music-container');
+const playbtn=document.querySelector('#play');
+const prevbtn=document.querySelector('#prev');
+const nextbtn=document.querySelector('#nex');
+const audio=document.querySelector('#audio');
+const progress=document.querySelector('.progress');
+const progressContainer=document.querySelector('.progress-container');
+const title=document.querySelector('#title');
+const cover=document.querySelector('#cover');
+const songs=['Song1','Song2','Song3'];//title of the songs
+let songIndex=2;
+loadSong(songs[songIndex])//initialise function of load songs
+function loadSong(song){
+  title.innerText=song;
+  audio.src=`music/${song}.mp3`;
+  cover.src=`image/${song}.jpg`;
+}
+
+function playSong(){
+  musicContainer.classList.add('play');
+  playbtn.querySelector('i.fa').classList.remove('fa-play');
+  playbtn.querySelector('i.fa').classList.add('fa-pause');
+  audio.play();
+}
+
+function pauseSong(){
+  musicContainer.classList.remove('play');
+  playbtn.querySelector('i.fa').classList.remove('fa-pause');
+  playbtn.querySelector('i.fa').classList.add('fa-play');
+  audio.pause();
+}
+//change songs
+function prevSong(){
+  songIndex--
+  if(songIndex<0){
+    songIndex=songs.length-1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+function nextSong(){
+  songIndex++
+  if(songIndex>songs.length-1){
+    songIndex=0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(e){
+  const {duration,currentTime}=e.srcElement;
+  const progressPercent=(currentTime/duration)*100;
+  progress.style.width=`${progressPercent}%`;
+
+}
+function setProgress(e){
+  const width=this.clientWidth;
+  const clickX=e.offsetX
+  const duration=audio.duration
+  audio.currentTime=(clickX/width)*duration;
+}
+playbtn.addEventListener('click',() => {
+  const isPlaying=musicContainer.classList.contains('play');
+  if(isPlaying){
+    pauseSong();
+  }
+  else{
+    playSong();
+  }
+})
+prevbtn.addEventListener('click',prevSong);
+nextbtn.addEventListener('click',nextSong);
+audio.addEventListener('timeupdate',updateProgress);
+progressContainer.addEventListener('click',setProgress);
+audio.addEventListener('ended',nextSong)
